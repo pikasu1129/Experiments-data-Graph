4ff4f09b0cc4e0c27f68edad68d1d509b7d7d1e9
pikasu1129 <ueda730@gmail.com>
2022-04-22 00:04:20 +0900

    複数画像からGIFを作成するプログラムを追加。 プログラムファイルと同じ階層に画像ファイルを置いておく必要がある。

---
+++ 複数画像からGif作成.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import glob
+
+files = sorted(glob.glob('./*.png'))  
+images = list(map(lambda file : Image.open(file) , files))
+images[0].save('image.gif' , save_all = True , append_images = images[1:] , duration = 400 , loop = 0)
